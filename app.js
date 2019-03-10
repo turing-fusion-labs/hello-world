121b96b17aabfa27e4abf59a28b1b0cdbe9c9b32
saurabhvaish <saurabhv@tm7.tech>
2019-03-10 14:22:20 -0400

    nodemon, removed extra files

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ var express = require('express'),
     Datastore = require('nedb'),
     bodyParser = require('body-parser');
 
-
 //EJS
 app.set('view engine', 'ejs')
-
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.listen(7000);
 
+console.log('Hellow World Test Program started on: ' + 7000);  // This is example of logging message in the console (black screen)
+
+// ---------- do not change above unless you know what you are doing :) -----------
 
 
 app.get('/', function(req, res) {
   res.render('pages/index');
 });
-
-
-console.log('RESTful API server started on: ' + 7000);  // This is example of logging message in the console (black screen)
