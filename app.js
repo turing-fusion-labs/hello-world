50e357087923bc4ba3dfafc3f85faf43424568e3
saurabhvaish <saurabhv@tm7.tech>
2019-03-15 13:34:55 -0400

    updates

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.listen(7000);
 
-console.log('Hellow World Test Program started on: ' + 7000);  // This is example of logging message in the console (black screen)
+console.log('Hellow World Test Program started on http://localhost:7000');  // This is example of logging message in the console (black screen)
 
 app.get('/', function(request, response) {
     response.render('index');
@@ -26,6 +26,8 @@ function calculateAge(yearOfBirth){
 
 
 app.post('/save', function(request, response) {
+
     var firstName = request.body.firstname;
+
     response.render('nextpage', { fname:firstName });
 });
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// function to calculate area of rectangle
 function areaOfRectangle(side1, side2) {
  var area = side1 * side2;
  return area;
@@ -10,6 +11,6 @@ var c = 4;
 var area1 = areaOfRectangle(a,b);
 var area2 = areaOfRectangle(b,c);
 
-
+//print calculated values
 console.log ( area1 );
 console.log ( area2 );
